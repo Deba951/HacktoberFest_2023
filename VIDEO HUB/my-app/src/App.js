304ad04e05d2041d8c526c5d87bb29378615614f
Arroew <aranyadas2013@gmail.com>
2023-10-04 13:42:42 +0530

    Created the Basic Outline of  Folder Structure In React

---
+++ VIDEO HUB/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
